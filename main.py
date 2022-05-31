a81fdd2059061bb9ed8756533a733b68095ed47a
KevinCostta <93344510+KevinCostta@users.noreply.github.com>
2022-05-31 01:34:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask("Amor")
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/cartao")
+def hello_cartao():
+    return render_template("cartao.html")
 
 app.run(host='0.0.0.0')
